6912d5ed044f3e02e417defa10035e6e5e2b35aa
Tommaso Venza <tommasovenza@gmail.com>
2020-06-09 01:32:30 +0200

    up

--- main.js
+++ main.js
@@ -1,23 +1,20 @@
-// // Nell’esercizio del pomeriggio dovremo scaricare un menu già completo nella parte html-css e dovremo aggiungere la funzionalità di 
-// apertura/chiusure del menu all’hamburger menu(visibile in versione mobile) utilizzando jQuery.
-// Il codice base è scaricabile da qui https://bitbucket.org/booleancareers/ex-hamburger-menu-base
+// Nell’esercizio del pomeriggio dovremo scaricare un menu già completo nella parte html-css 
+// e dovremo aggiungere la funzionalità di apertura/chiusure del menu all’hamburger menu
+// (visibile in versione mobile) utilizzando jQuery.
 
-// Nome repo: js-jq-hamburger
 
-// Buon lavoro! :)
 
 
 // il click fa apparire il menu nascosto e...
-$('.fa-bars').click(function() {
+$('.fa-bars').click(function () {
 
-        $('.hamburger-menu').show();
-    
-}); 
+    $('.hamburger-menu').show();
+
+});
 
 // ...fa sparire il menu
-$('.fa-times').click(function() {
+$('.fa-times').click(function () {
 
     $('.hamburger-menu').hide();
 
-}); 
-
+});
